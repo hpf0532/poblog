331af1a989d8751c5ff2811637db91b2a5cf1054
hpf <hpf1992@126.com>
2018-01-01 12:25:22 +0800

    base html created.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return '<h1>Hello, world!</h1>'
+    user = {'nickname' : 'hpf'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},\
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
